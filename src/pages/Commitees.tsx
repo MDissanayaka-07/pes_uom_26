429cd5eb90426fba246dc1d552a20788ba8f87aa
sawaniThiwandika <thiwandika.whs02@gmail.com>
2026-03-13 09:01:00 +0530

    change image path

--- src/pages/Commitees.tsx
+++ src/pages/Commitees.tsx
@@ -2,6 +2,38 @@ import { useState } from "react";
 import MemberCard from "../components/MemberCard";
 import "../styles/pages.css";
 import kavishka from "../../public/committee/kavishka.png"
+import nadun from "../../public/committee/nadun.png"
+import saneru from "../../public/committee/saneru.png"
+import lasindu from "../../public/committee/lasindu.png"
+import ishan from "../../public/committee/ishan.png"
+import vidura from "../../public/committee/vidura.jpeg"
+import dulsika from "../../public/committee/dulsika.jpeg"
+import mahesha from "../../public/committee/mahesha.jpg"
+import pabasara from "../../public/committee/pabasara.jpeg"
+import shakya from "../../public/committee/shakya.webp"
+import seynath from "../../public/committee/seynath.jpg"
+import vihanga from "../../public/committee/vihanga.webp"
+import hasitha from "../../public/committee/hasitha.jpg"
+import anupa from "../../public/committee/anupa.png"
+import kivira from "../../public/committee/kivira.jpeg"
+import chamix from "../../public/committee/chamix.jpeg"
+import easha from "../../public/committee/easha.jpeg"
+import achintha from "../../public/committee/achintha.jpg"
+import dilini from "../../public/committee/dilni.jpeg"
+import thuvarahan from "../../public/committee/thuvarahan.jpg"
+import thisul from "../../public/committee/thisul.jpg"
+import shalani from "../../public/committee/shalani.jpg"
+import vindya from "../../public/committee/vindya.jpeg"
+import harshani from "../../public/committee/harshani.jpg"
+import dulasha from "../../public/committee/dulasha.jpg"
+import raveesha from "../../public/committee/raveesha.jpg"
+import charithma from "../../public/committee/charithma.png"
+import okindu from "../../public/committee/okindu.jpg"
+import sawani from "../../public/committee/sawani.jpeg"
+import chanupa from "../../public/committee/chanupa.jpg"
+import madhuwantha from "../../public/committee/madhuwantha.jpeg"
+
+
 
 export default function Committees(){
 
@@ -13,44 +45,44 @@ const committees = [
 title:"Executive Committee",
 members:[
 {name:"Kavishka Lakshan",role:"President",image:kavishka},
-{name:"Nadun Walaketiya",role:"Vice President",image:"src/assets/committee/nadun.png"},
-{name:"Saneru Weerasinghe",role:"Secretary",image:"src/assets/committee/saneru.png"},
-{name:"Lasindu Shaluka",role:"Treasurer",image:"src/assets/committee/lasindu.png"},
-{name:"Ishan Sasnaka",role:"Webmaster",image:"src/assets/committee/ishan.png"}
+{name:"Nadun Walaketiya",role:"Vice President",image:nadun},
+{name:"Saneru Weerasinghe",role:"Secretary",image:saneru},
+{name:"Lasindu Shaluka",role:"Treasurer",image:lasindu},
+{name:"Ishan Sasnaka",role:"Webmaster",image:ishan}
 ]
 },
 
 {
 title:"Public Relations",
 members:[
-{name:"Vidura Jayawardhene",role:"Lead",image:"src/assets/committee/vidura.jpeg"},
-{name:"Dulsika Mendis",role:"Member",image:"src/assets/committee/dulsika.jpeg"},
-{name:"Mahesha Weerasinghe",role:"Member",image:"src/assets/committee/mahesha.jpg"}
+{name:"Vidura Jayawardhene",role:"Lead",image:vidura},
+{name:"Dulsika Mendis",role:"Member",image:dulsika},
+{name:"Mahesha Weerasinghe",role:"Member",image:mahesha}
 ]
 },
 
 {
 title:"Organizing",
  members: [
-        { name: "Pabasara Piyumal", role: "Lead", image: "src/assets/committee/pabasara.jpeg" },
-        { name: "Shakya Marasingha", role: "Member", image: "src/assets/committee/shakya.webp" },
-        { name: "Seynath Chathwara Suraweera", role: "Member", image: "src/assets/committee/seynath.jpg" },
-        { name: "Sembakuttige Vihanga Kaushal", role: "Member", image: "src/assets/committee/vihanga.webp" },
-        { name: "Hasitha Rupasinghe", role: "Member", image: "src/assets/committee/hasitha.jpg" },
-        { name: "P.K. Anupa Minojitha", role: "Member", image: "src/assets/committee/anupa.png" },
-        { name: "Kivira Vehan Gammaddegoda", role: "Member", image: "src/assets/committee/kivira.jpeg" }
+        { name: "Pabasara Piyumal", role: "Lead", image: pabasara },
+        { name: "Shakya Marasingha", role: "Member", image: shakya },
+        { name: "Seynath Chathwara Suraweera", role: "Member", image:seynath },
+        { name: "Sembakuttige Vihanga Kaushal", role: "Member", image:vihanga },
+        { name: "Hasitha Rupasinghe", role: "Member", image:hasitha },
+        { name: "P.K. Anupa Minojitha", role: "Member", image: anupa},
+        { name: "Kivira Vehan Gammaddegoda", role: "Member", image: kivira }
       ]
 },
 
 {
 title:"Creative Design",
 members:[
-  {name:"Chamiru Gajasinghe",role:"Lead",image:"src/assets/committee/chamix.jpeg"},
-  {name:"Easha Wanniarachchi",role:"Member",image:"src/assets/committee/easha.jpeg"},
-  {name:"Achintha Niroshan",role:"Member",image:"src/assets/committee/achintha.jpg"},
-  {name:"Dilni Nimanya Muthukuda",role:"Member",image:"src/assets/committee/dilni.jpeg"},
-  {name:"Thuvarahan Thayalan",role:"Member",image:"src/assets/committee/thuvarahan.jpg"},
-  {name:"B.G. Thisul Dulneth",role:"Member",image:"src/assets/committee/thisul.jpg"}
+  {name:"Chamiru Gajasinghe",role:"Lead",image:chamix},
+  {name:"Easha Wanniarachchi",role:"Member",image:easha},
+  {name:"Achintha Niroshan",role:"Member",image:achintha},
+  {name:"Dilni Nimanya Muthukuda",role:"Member",image:dilini},
+  {name:"Thuvarahan Thayalan",role:"Member",image:thuvarahan},
+  {name:"B.G. Thisul Dulneth",role:"Member",image:thisul}
 ]
 },
 
@@ -58,28 +90,28 @@ members:[
 {
 title:"Editorial",
 members:[
-{name:"Shalani Ambalangodage",role:"Lead",image:"src/assets/committee/shalani.jpg"},
-{name:"Vindya Nilushika",role:"Member",image:"src/assets/committee/vindya.jpeg"},
-{name:"Harshani Lakmali",role:"Member",image:"src/assets/committee/harshani.jpg"},
-{name:"Dulasha Perera",role:"Member",image:"src/assets/committee/dulsha.jpg"},
-{name:"Raveesha Samarasinghe",role:"Member",image:"src/assets/committee/raveesha.jpg"}
+{name:"Shalani Ambalangodage",role:"Lead",image:shalani},
+{name:"Vindya Nilushika",role:"Member",image:vindya},
+{name:"Harshani Lakmali",role:"Member",image:harshani},
+{name:"Dulasha Perera",role:"Member",image:dulasha},
+{name:"Raveesha Samarasinghe",role:"Member",image:raveesha}
 ]
 },
 
 {
 title:"Finance",
 members:[
-{name:"Charithma Dewmi",role:"Lead",image:"src/assets/committee/charithma.png"},
-{name:"Okindu Fernando",role:"Member",image:"src/assets/committee/okindu.jpg"}
+{name:"Charithma Dewmi",role:"Lead",image:charithma},
+{name:"Okindu Fernando",role:"Member",image:okindu}
 ]
 },
 
 {
 title:"Web Development",
 members: [
-        { name: "Sawani Thiwandika", role: "Lead", image: "src/assets/committee/sawani.png" },
-        { name: "Chanupa Hansaja Vithanage", role: "Member", image: "src/assets/committee/chanupa.jpg" },
-        { name: "Madhuwantha Dissanayaka", role: "Member", image: "src/assets/committee/madhuwantha.jpeg" }
+        { name: "Sawani Thiwandika", role: "Lead", image: sawani },
+        { name: "Chanupa Hansaja Vithanage", role: "Member", image: chanupa},
+        { name: "Madhuwantha Dissanayaka", role: "Member", image: madhuwantha}
       ]
 }
 
