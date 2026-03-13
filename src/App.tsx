cf2ac123e3b0e72ba2e5badd7c78783a08e5678a
Sasindu Madhuwantha <madhudissa07@gmail.com>
2026-03-13 07:45:05 +0530

    feat(component): implement awards panel component

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,20 @@ import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
 import About from "./pages/About.tsx";
 import Committees from "./pages/Commitees.tsx";
+import AwardsPanel from "./components/AwardsPanel.tsx";
 
 function App() {
   return (
     <>
       <Navbar />
+      <AwardsPanel />
 
       <Routes>
 
         <Route path="/" element={<About />} />
         <Route path="/about" element={<About />} />
         <Route path="/committees" element={<Committees />} />
-        <Route path="/achievements" element={<Achievements />} />
-        <Route path="/achievement/:id" element={<AchievementDetails />} />
+        
 
       </Routes>
 
